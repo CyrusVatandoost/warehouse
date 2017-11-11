793317936e1e1a9d1fd0847acb72b5c54c92dcfe
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-11-11 19:02:10 +0800

    Add Comments

--- index.php
+++ index.php
@@ -59,29 +59,33 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">Logo</a>
+      <a class="navbar-brand" href="#">WareHouse</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="#">My Projects</a></li>
+        <li><a href="#">Something</a></li>
+        <li><a href="#">Something</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       </ul>
     </div>
   </div>
 </nav>
-  
-<div class="container-fluid text-center">    
+
+<div class="container-fluid text-center">
   <div class="row content">
+
+    <!-- left sidenav -->
     <div class="col-sm-2 sidenav">
       <p><a href="#">Link</a></p>
       <p><a href="#">Link</a></p>
       <p><a href="#">Link</a></p>
     </div>
+
+    <!-- body -->
     <div class="col-sm-8 text-left"> 
       <h1>Welcome</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
@@ -89,6 +93,8 @@
       <h3>Test</h3>
       <p>Lorem ipsum...</p>
     </div>
+
+    <!-- right sidenav -->
     <div class="col-sm-2 sidenav">
       <div class="well">
         <p>ADS</p>
@@ -97,11 +103,13 @@
         <p>ADS</p>
       </div>
     </div>
+
   </div>
 </div>
 
+<!-- footer -->
 <footer class="container-fluid text-center">
-  <p>Footer Text</p>
+  <p>ImagineWare Solutions 2017</p>
 </footer>
 
 </body>
