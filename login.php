c7361ac945ab1cbd8a5fa4c6902b7c56216db9bd
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-11-11 21:04:10 +0800

    Edit Login
    Changed the layout of the login.php page.

--- login.php
+++ login.php
@@ -4,31 +4,56 @@
 	Last modified:	2016 11 01
 -->
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="UTF-8">
 	<title>T3D WareHouse | Login</title>
-	<link rel="stylesheet" type="text/css" href="css/div.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/div.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-<style type="text/css">
-	
-	body {
-		background-color: #e1ffb1;
-	}
-
-</style>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<?php
-
-?>
 <body>
 
-	<div class="parent">
+<!-- header -->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">WareHouse</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="projects.php">Projects</a></li>
+        <li><a href="account.php">Account</a></li>
+        <li><a href="settings.php">Settings</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-		<div class="header">
-			Header
-		: Hello</div>
+<div class="container-fluid text-center">
+  <div class="row content">
 
+    <!-- left sidenav -->
+    <div class="col-sm-2 sidenav">
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+    </div>
+
+    <!-- body -->
+    <div class="col-sm-8 text-left"> 
 		<div class="block-center">
 			<form>
 				<table class="form">
@@ -40,8 +65,25 @@
 				</table>	
 			</form>
 		</div>
+    </div>
 
-	</div>
+    <!-- right sidenav -->
+    <div class="col-sm-2 sidenav">
+      <div class="well">
+        <p>ADS</p>
+      </div>
+      <div class="well">
+        <p>ADS</p>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<!-- footer -->
+<footer class="container-fluid text-center">
+  <p>ImagineWare Solutions 2017</p>
+</footer>
 
 </body>
 </html>
