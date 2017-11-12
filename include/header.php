56838a8cc4c331b9d3ed61f709b103548291b373
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-11-12 21:59:16 +0800

    Add Include Files
    Added:
    - a folder for include files "/warehouse/include"
    - head_style.php for importing the css styles and bootstrap
    - header.php for the header nav bar
    - footer.php for the footer
    - folder: "warehouse/projects" for the projects page. "My Projects" and "All Projects" pages will be here.

---
+++ include/header.php
@@ -0,0 +1,38 @@
+<!-- header -->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">WareHouse</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+
+      	<?php
+      		if($_SERVER['REQUEST_URI'] == "/warehouse/index.php")
+      			echo "<li class=active><a href=/warehouse/index.php>Home</a></li>";
+      		else
+      			echo "<li><a href=/warehouse/index.php>Home</a></li>";
+
+      		if($_SERVER['REQUEST_URI'] == "/warehouse/projects.php")
+      			echo "<li class=active><a href=/warehouse/projects.php>Projects</a></li>";
+      		else
+      			echo "<li><a href=/warehouse/projects.php>Projects</a></li>";
+
+      		if($_SERVER['REQUEST_URI'] == "/warehouse/account.php")
+      			echo "<li class=active><a href=/warehouse/account.php>Account</a></li>";
+      		else
+      			echo "<li><a href=/warehouse/account.php>Account</a></li>";
+        ?>
+
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="/warehouse/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
