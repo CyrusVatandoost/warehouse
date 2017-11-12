fb3034089dd5165ef87bab5c2ae4e91b7cef3d4c
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-11-13 01:15:20 +0800

    Edit Account Page

--- account.php
+++ account.php
@@ -19,19 +19,74 @@
 
     <!-- left sidenav -->
     <div class="col-sm-2 sidenav">
-      <p><a href="#">Edit your profile</a></p>
+      <p><a href="#">Edit Profile</a></p>
     </div>
 
     <!-- body -->
     <div class="col-sm-8 text-left"> 
-      <h1>Account</h1>
+      <br>
       <img src="res/account.png" width="150" height="150">
-      <p>Name: Juan Dela Cruz</p>
-      <p>Type of User: Member</p>
+      <h1>Juan Dela Cruz</h1>
+      <p><a href="#">juandelacruz@dlsu.edu.ph</a>
+      <p>T3D Member
     
       <hr>
-      <h3>About Myself</h3>
       <p>Hi I am Juan and I am a third year college student taking up bachelor of science computer science in De La Salle University</p>
+
+      <hr>
+      <h3>Personal Projects</h3>
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="thumbnail">
+            <img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg" />
+            <div class="caption">
+              <h3>
+                <a href="/warehouse/project.php">Thumbnail label</a>
+              </h3>
+              <p>
+                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+              </p>
+              <p>
+                <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="thumbnail">
+            <img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg" />
+            <div class="caption">
+              <h3>
+                <a href="/warehouse/project.php">Thumbnail label</a>
+              </h3>
+              <p>
+                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+              </p>
+              <p>
+                <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="thumbnail">
+            <img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/output/sports-q-c-600-200-1.jpg" />
+            <div class="caption">
+              <h3>
+                <a href="/warehouse/project.php">Thumbnail label</a>
+              </h3>
+              <p>
+                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+              </p>
+              <p>
+                <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
    
     <!-- right sidenav -->
