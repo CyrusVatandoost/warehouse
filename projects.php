d67669ad0dbea1be9e2211b2a052987f37361180
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-11-12 11:53:04 +0800

    Add Thumbnails
    Added badges and sample thumbnails on projects.php

--- projects.php
+++ projects.php
@@ -55,10 +55,125 @@
     <!-- body -->
     <div class="col-sm-8 text-left"> 
       <h1>Projects</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+      <!-- badge -->
+      <ul class="nav nav-pills">
+        <li class="active">
+           <a href="#"> <span class="badge pull-right">42</span>My Projects</a>
+        </li>
+        <li>
+           <a href="#"> <span class="badge pull-right">16</span>All Projects</a>
+        </li>
+      </ul>
+
       <hr>
-      <h3>Test</h3>
-      <p>Lorem ipsum...</p>
+
+        <div class="row">
+
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg" />
+              <div class="caption">
+                <h3>
+                  Thumbnail label
+                </h3>
+                <p>
+                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+                <p>
+                  <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg" />
+              <div class="caption">
+                <h3>
+                  Thumbnail label
+                </h3>
+                <p>
+                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+                <p>
+                  <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/output/sports-q-c-600-200-1.jpg" />
+              <div class="caption">
+                <h3>
+                  Thumbnail label
+                </h3>
+                <p>
+                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+                <p>
+                  <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg" />
+              <div class="caption">
+                <h3>
+                  Thumbnail label
+                </h3>
+                <p>
+                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+                <p>
+                  <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg" />
+              <div class="caption">
+                <h3>
+                  Thumbnail label
+                </h3>
+                <p>
+                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+                <p>
+                  <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/output/sports-q-c-600-200-1.jpg" />
+              <div class="caption">
+                <h3>
+                  Thumbnail label
+                </h3>
+                <p>
+                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+                <p>
+                  <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
     </div>
 
     <!-- right sidenav -->
