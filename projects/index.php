84fb6582a274ed1465b172f4a50a446b6ef77ea2
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-11-15 01:09:02 +0800

    Split Projects File
    The Projects page can be found in a folder. It will automatically go to index and based on the user's last visit, the page will get the projects of the user or all the projects.

--- projects/index.php
+++ projects/index.php
@@ -3,17 +3,40 @@
 	Created on:		2016 11 12
 	Last modified:	2016 11 12
 -->
+<?php
+  session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>T3D WareHouse | Projects</title>
   <?php require realpath($_SERVER['DOCUMENT_ROOT'])."/warehouse/include/head_style.php"; ?>
 </head>
+<script type="text/javascript">
+  function setViewMy() {
+    $_SESSION['view'] = "my";
+    header("Refresh:0");
+  }
+  function setViewAll() {
+    $_SESSION['view'] = "all";
+    header("Refresh:0");
+  }
+</script>
 <body>
+
+<?php
+  if (isset($_SESSION['view'])) {
+    $view = $_SESSION['view'];
+  }
+  else {
+    $view = "all";
+  }
+?>
 
 <!-- header -->
 <?php require realpath($_SERVER['DOCUMENT_ROOT'])."/warehouse/include/header.php"; ?>
 
+<!-- body -->
 <div class="container-fluid text-center">
   <div class="row content">
 
@@ -24,64 +47,69 @@
       <p><a href="#">Link</a></p>
     </div>
 
-    <!-- body -->
-    <div class="col-sm-8 text-left"> 
-      <h1>Projects</h1>
-
-      <!-- badge -->
-      <ul class="nav nav-pills">
-        <li class="active">
-           <a href="#"> <span class="badge pull-right">3</span>My Projects</a>
-        </li>
-        <li>
-           <a href="all.php"> <span class="badge pull-right">7</span>All Projects</a>
-        </li>
-      </ul>
-
-      <hr>
-
+    <!-- center body -->
+    <div class="col-sm-8 text-left">
       <div class="row">
-        <div class="col-md-4">
-          <div class="thumbnail">
-            <img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg" />
-            <div class="caption">
-              <h3>
-                <a href="/warehouse/project.php">Thumbnail label</a>
-              </h3>
-              <p>
-                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="thumbnail">
-            <img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg" />
-            <div class="caption">
-              <h3>
-                <a href="/warehouse/project.php">Thumbnail label</a>
-              </h3>
-              <p>
-                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="thumbnail">
-            <img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/output/sports-q-c-600-200-1.jpg" />
-            <div class="caption">
-              <h3>
-                <a href="/warehouse/project.php">Thumbnail label</a>
-              </h3>
-              <p>
-                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+        <p>
 
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg" />
+              <div class="caption">
+                <h3>
+                  <a href="/warehouse/project.php">Thumbnail label</a>
+                </h3>
+                <p>
+                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg" />
+              <div class="caption">
+                <h3>
+                  <a href="/warehouse/project.php">Thumbnail label</a>
+                </h3>
+                <p>
+                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg" />
+              <div class="caption">
+                <h3>
+                  <a href="/warehouse/project.php">Thumbnail label</a>
+                </h3>
+                <p>
+                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg" />
+              <div class="caption">
+                <h3>
+                  <a href="/warehouse/project.php">Thumbnail label</a>
+                </h3>
+                <p>
+                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </p>
+      </div>
     </div>
 
     <!-- right sidenav -->
