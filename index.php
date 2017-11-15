84bdb4f2862ccf04862fbb006217a250d78718b1
Marl Christian Ricanor <32050672+Marlcelo@users.noreply.github.com>
2017-11-15 11:10:10 +0800

    Test Commit

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
               <img alt="Carousel Bootstrap Third" src="http://lorempixel.com/output/sports-q-c-1600-500-3.jpg" />
               <div class="carousel-caption">
                 <h4>
-                  Third Thumbnail label
+                  WHATEVER
                 </h4>
                 <p>
                   Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
