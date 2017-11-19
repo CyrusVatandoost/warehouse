a86331b216f54a21799bb3787b0d21a66526d96b
Chelsey <chelsey_medina@dlsu.edu.ph>
2017-11-19 21:20:50 +0800

    Add Temporary Info
    these shall be changed

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,34 @@
+<!--
+	Author:			Chelsey
+	Created on:		2017 11 12
+	Last modified:	2017 11 19
+-->
 <footer class="container-fluid text-center">
-  <p>ImagineWare Solutions 2017</p>
+  
+	<!-- first column -->
+	<p class="left">
+  
+		TED HOUSE<br><br>
+
+		About TED House<br>
+		inquire@te3dhouse.edu.ph<br>
+		(02) 809 7392<br>
+		LTI Spine Road, Laguna Blvd. Binan<br>
+	</p>
+	
+	<!-- second column -->
+	<p class="center">
+  
+		WAREHOUSE<br><br>
+
+		Home<br>
+		Projects<br>
+		Contact Us<br>
+		Members Sign In<br>
+  
+	</p>
+	
+	<!-- third column -->
+	<p class="right">ImagineWare Solutions, Inc. 2017</p>
+
 </footer>
