d7312eeb50f3e890a61df91bcf46ebfd91d4f3dc
Chelsey <chelsey_medina@dlsu.edu.ph>
2017-11-20 20:24:05 +0800

    Footer in table form

--- include/footer.php
+++ include/footer.php
@@ -4,31 +4,36 @@
 	Last modified:	2017 11 19
 -->
 <footer class="container-fluid text-center">
-  
-	<!-- first column -->
-	<p class="left">
-  
-		TED HOUSE<br><br>
-
-		About TED House<br>
-		inquire@te3dhouse.edu.ph<br>
-		(02) 809 7392<br>
-		LTI Spine Road, Laguna Blvd. Binan<br>
-	</p>
 	
-	<!-- second column -->
-	<p class="center">
-  
-		WAREHOUSE<br><br>
-
-		Home<br>
-		Projects<br>
-		Contact Us<br>
-		Members Sign In<br>
-  
-	</p>
-	
-	<!-- third column -->
-	<p class="right">ImagineWare Solutions, Inc. 2017</p>
+	<center>
+		<!--Table that contains info about TE3D and the WAREHOUSE-->
+		
+		<table class="tablemodification">
+			<tr>
+				<th><h4>TE<sup>3</sup>D HOUSE</h4></th>
+				<th><h4>WAREHOUSE</h4></th>
+			</tr>
+			<tr>
+				<th><a href="">About TED House</a></th>
+				<th><a href="">Home</a></th>
+			</tr>
+			
+			<tr>
+				<th>inquire@te3dhouse.edu.ph</th>
+				<th><a href="">Projects</a></th>
+			</tr>
+			
+			<tr>
+				<th>(02) 809 7392</th>
+				<th><a href="">Contact Us</a></th>
+			</tr>
+			
+			<tr>
+				<th>LTI Spine Road, Laguna Blvd. Binan</th>
+				<th><a href="">Members Sign In</a></th>
+				<th>ImagineWare Solutions, Inc. 2017</th>
+			</tr>
+		</table>
+	</center>
 
 </footer>
