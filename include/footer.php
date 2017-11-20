91a1d2da6e803e032a4084655f4b7d3962ce6d12
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-11-20 21:25:53 +0800

    Initial Laravel Commit
    Changed:
    - Everything.

--- include/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="container-fluid text-center">
-  <p>ImagineWare Solutions 2017</p>
-</footer>
--- login/loginheader.php
+++
@@ -1,6 +0,0 @@
-<?php
-//PUT THIS HEADER ON TOP OF EACH UNIQUE PAGE
-session_start();
-if (!isset($_SESSION['email'])) {
-    return header("location:login/main_login.php");
-}
