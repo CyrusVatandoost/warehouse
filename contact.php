cae80169482fe51b1f266029c648fb5d8fdf5b63
Marl Christian Ricanor <32050672+Marlcelo@users.noreply.github.com>
2017-11-20 23:24:52 +0800

    Added Fields for Contact Us
    I have added the following in the forms:
    >Name
    >Email
    >Topic
    >Comment
    
    I have also placed the submit button as well as a reset button
    
    I havent added any design yet. Hoping I could have a template to follow for the CSS. Thanks!

--- contact.php
+++ contact.php
@@ -7,6 +7,7 @@
 <head>
 	<title>T3D WareHouse | Contact Us</title>
   <?php require realpath($_SERVER['DOCUMENT_ROOT'])."/warehouse/include/style.php"; ?>
+
 </head>
 <body>
 
@@ -29,6 +30,21 @@
         <!--
           body here
         -->
+
+        <h2>Contact Us</h2><br><br><br><br>
+
+        <form>
+          Name: &nbsp;&nbsp;&nbsp; <input type="text" name="name" ><br><br><br>
+
+          E-mail: &nbsp;&nbsp; <input type="text" name="email" ><br><br><br>
+
+          Topic: &nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="topic"><br><br><br>
+
+          Comment: <textarea name="comment" rows="6" cols="25"></textarea><br><br>
+
+          <input type="submit" value="Send"><span style="display:inline-block; width: 20px;"></span><input type="reset" value="Clear">
+        </form>
+
       </p>
     </div>
 
