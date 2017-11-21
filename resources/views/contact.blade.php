3effd5ebb4e1801ba2124dafef8687b2d55c73a0
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-11-21 22:04:28 +0800

    Add Form in Contact Us

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,7 +8,27 @@
   @endsection
 
 @section('body')
-  Home Body
+  <p>
+    <form>
+      <table class="form">
+        <tr>
+          <td>Name:
+          <td><input type="text" name="name" >
+        <tr>
+          <td>E-mail:
+          <td><input type="text" name="email" >
+        <tr>
+          <td>Topic:
+          <td><input type="text" name="topic">
+        <tr>
+          <td>Comment:
+          <td><textarea name="comment" rows="6"></textarea>
+        <tr>
+          <td><input type="submit" value="Send">
+          <td><input type="reset" value="Clear">
+      </table>
+    </form>
+  </p>
   @endsection
 
 @section('right-sidebar')
