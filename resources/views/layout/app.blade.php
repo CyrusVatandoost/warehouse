5074d03ccdf9d3913af2d1fd8788efb1b65234ee
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-11-21 21:16:50 +0800

    Apply Footer Table and Style

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -78,8 +78,29 @@
 
 </div>
 
+<!-- footer -->
 <footer class="container-fluid text-center">
-  <p>ImagineWare Solutions 2017</p>
+  <center>
+    <!--Table that contains info about TE3D and the WAREHOUSE-->
+    <table class="footer">
+      <tr>
+        <td><h4>TE<sup>3</sup>D HOUSE</h4>
+        <td><h4>WAREHOUSE</h4>
+      <tr>
+        <td><a href="">About TED House</a>
+        <td><a href="">Home</a>
+      <tr>
+        <td>inquire@te3dhouse.edu.ph
+        <td><a href="">Projects</a>
+      <tr>
+        <td>(02) 809 7392
+        <td><a href="">Contact Us</a>
+      <tr>
+        <td>LTI Spine Road, Laguna Blvd. Binan
+        <td><a href="">Members Sign In</a>
+        <td>ImagineWare Solutions, Inc. 2017
+    </table>
+  </center>
 </footer>
 
 </body>
