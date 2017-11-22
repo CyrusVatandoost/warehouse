97f40bea24cf2985eef7ff6dc6cf0476cc34530c
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-11-22 11:48:09 +0800

    Add Links in Footer

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
 Route::get('/login', function () {
     return view('login');
 });
