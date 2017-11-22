3d52b5f1b62de90cb4937d892db5efd8f3632cb7
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-11-22 11:44:24 +0800

    Add New Project Modal

---
+++ resources/views/modals/new_project.blade.php
@@ -0,0 +1,32 @@
+<div class="modal fade" id="modal-container-new-project" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<form role="form">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+						×
+					</button>
+					<h4 class="modal-title" id="myModalLabel">
+						New Project
+					</h4>
+				</div>
+				<div class="modal-body">
+					<div class="form-group">
+						<label for="input_name">
+							Name
+						</label>
+						<input type="text" class="form-control" id="input_name" />
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">
+						Cancel
+					</button> 
+					<button type="submit" class="btn btn-primary">
+						Create Project
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
