01bc9637ea31d1bb4d43fd81a1da3df77d0d09ac
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-11-27 04:33:16 +0800

    Fix Modal UI

--- resources/views/modals/delete_project.blade.php
+++ resources/views/modals/delete_project.blade.php
@@ -3,8 +3,8 @@
 		<div class="modal-content">
 
 			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
 				<h4 class="modal-title" id="myModalLabel">Delete Project</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
 			</div>
 
       <div class="modal-body">
