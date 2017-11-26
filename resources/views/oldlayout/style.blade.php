4a482ec374c7d0e9d2d2aaaa00b7531f549a6618
xandraureta <31879731+xandraureta@users.noreply.github.com>
2017-11-27 02:18:18 +0800

    Change Bootstrap4 Footer

--- resources/views/oldlayout/style.blade.php
+++ resources/views/oldlayout/style.blade.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link href="{{ asset('css/style.css') }}" media="all" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/old.css') }}" media="all" rel="stylesheet" type="text/css" />
 <link href="{{ asset('css/map.css') }}" media="all" rel="stylesheet" type="text/css" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
