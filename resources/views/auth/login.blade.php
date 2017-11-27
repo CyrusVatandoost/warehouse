1dabec8972e9e4c67f65c125eda0d7cb8b411953
xandraureta <31879731+xandraureta@users.noreply.github.com>
2017-11-28 00:41:19 +0800

    Change Auth Pages To Bootstrap 4.0

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,188 @@
-@extends('layouts.app')
+@extends('layout.auth')
+
 
 @section('content')
+<style type="text/css">
+    @import url("//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css");
+    .login-block{
+        float:left;
+        width:100%;
+        padding : 50px 0;
+    }
+    .help-block{
+        color: red;
+    }
+    .banner-sec{
+        background-size:cover;    
+        min-height:500px; 
+        border-radius: 0 10px 10px 0; 
+        padding:0;
+    }
+    .container{
+        background: #555; 
+        border-radius: 10px; 
+    }
+    .carousel-inner{
+        border-radius:0 10px 10px 0;
+    }
+    .carousel-caption{
+        text-align:left; left:5%;
+    }
+    .login-sec{
+        padding: 50px 30px; position:relative;
+    }
+    .login-sec .copy-text{
+        position:absolute; 
+        width:80%; 
+        bottom:20px; 
+        font-size:13px; 
+        text-align:center;
+    }
+    .login-sec .copy-text i{
+        color:#f5f5f5;
+    }
+    .login-sec .copy-text a{
+        color:#f5f5f5;
+    }
+    .login-sec h2{
+        margin-bottom:30px;
+        font-weight:200; 
+        font-size:30px; 
+        color:#f5f5f5;
+    }
+    .login-sec h2:after{
+        content:" "; 
+        width:100px; 
+        height:1px; 
+        background:#f5f5f5; 
+        display:block; 
+        margin-top:20px; 
+        border-radius:3px; 
+        margin-left:auto;
+        margin-right:auto
+    }
+    .btn-login{
+        background: #f5f5f5; 
+        color:#555;
+        font-weight:600;
+    }
+
+    .btn-link, label{
+         color:#f5f5f5 !important;
+    }
+
+    .banner-text{
+        width:70%; 
+        position:absolute; 
+        bottom:40px; 
+        padding-left:20px;
+        border-radius: 5px;
+        background: rgba(1,1,1,0.5);
+    }
+    .banner-text h2{
+        color: white; 
+        font-weight:500;
+    }
+    .banner-text h2:after{
+        content:" "; 
+        width:100px; 
+        height:2px; 
+        background: white; 
+        display:block; 
+        margin-top:10px; 
+        border-radius:3px;
+    }
+    .banner-text p{
+        color: white;
+    }
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+</style>
+<section class="login-block">
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+        <div class="col-md-4 login-sec">
+            <h2 class="text-center">Login Now</h2>
+            <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label for="email" class="control-label">E-Mail Address</label>
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                        @if ($errors->has('email'))
+                            <span class="help-block font-italic">
+                                {{ $errors->first('email') }}
+                            </span>
+                        @endif
+                </div>
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <label for="password" class="control-label">Password</label>
+                    <input id="password" type="password" class="form-control" name="password" required>
+                        @if ($errors->has('password'))
+                            <span class="help-block font-italic">
+                                {{ $errors->first('password') }}
+                            </span>
+                        @endif
+                </div>
+                <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                            </label>
                         </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                    </div>
+                </div>
+                <div class="form-check">
+                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                        <i> Forgot Your Password? </i>
+                    </a>
+                    <button type="submit" class="btn btn-login float-right">
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
+        <div class="col-md-8 banner-sec">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <img class="d-block img-fluid" src="storage/people-coffee-tea-meeting.jpg" alt="First slide">
+                        <div class="carousel-caption d-none d-md-block">
+                        <div class="banner-text">
+                            <h2>This is Heaven</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+                        </div>  
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block img-fluid" src="storage/people-woman-coffee-meeting.jpg" alt="First slide">
+                        <div class="carousel-caption d-none d-md-block">
+                        <div class="banner-text">
+                            <h2>This is Heaven</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+                        </div>  
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
+                    </div>
+                     <div class="carousel-item">
+                        <img class="d-block img-fluid" src="storage/pexels-photo.jpg" alt="First slide">
+                        <div class="carousel-caption d-none d-md-block">
+                        <div class="banner-text">
+                            <h2>This is Heaven</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+                        </div>  
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+</section>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,182 @@
-@extends('layouts.app')
+@extends('layout.auth')
+
 
 @section('content')
+<style type="text/css">
+    @import url("//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css");
+
+    body, html{
+        height: 100%;
+    }
+
+    .main{
+        padding : 50px 0;
+    }
+
+    h2 { 
+        border-bottom: 1px solid #f5f5f5; 
+        padding-bottom: 5px;  
+        font-weight:200; 
+    }
+
+    .form-group{
+        margin-bottom: 10px;
+    }
+
+    label{
+        margin-bottom: 4px;
+    }
+
+    .form-control {
+        height: auto !important;
+        padding: 8px 12px !important;
+    }
+
+    .input-group{
+        background: #f5f5f5;
+        border-radius: 5px;
+    }
+   
+    .main-center{
+        margin-top: 30px;
+        margin: 0 auto;
+        padding: 20px 40px;
+        background:#555;
+        color: #f5f5f5;
+        text-shadow: none;
+        border-radius: 10px;
+    }
+
+    .btn-login{
+        background: #f5f5f5; 
+        color: #555; 
+        font-weight:600;
+        margin-top: 5px;
+        border-radius: 5px;
+    }
+
+    .btn-link{
+        color:#f5f5f5 !important;
+    }
+
+    .help-block font-italic{
+        color: red;
+    }
+
+</style>
+<div class="container">
+    <div class="row main">
+        <div class="main-center">
+        <h2><center>Register</center></h2>
+            <div class="row">
+             <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                {{ csrf_field() }}
+
+                <div class="form-row">
+                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }} col-md-5">
+                        <label for="first_name" class="control-label">First Name</label>
+                            <div class="input-group">
+                                <input id="first_name" type="text" class="form-control" name="first_name" value="{{ old('first_name') }}" required autofocus>
+                                @if ($errors->has('first_name'))
+                                    <span class="help-block font-italic">
+                                        {{ $errors->first('first_name') }}
+                                    </span>
+                                @endif
+                            </div>
+                    </div>
+
+                    <div class="form-group{{ $errors->has('middle_initial') ? ' has-error' : '' }} col-md-2">
+                        <label for="middle_initial" class="control-label">M.I.</label>
+                        <div class="input-group">
+                            <input id="middle_initial" type="text" class="form-control" name="middle_initial" value="{{ old('middle_initial') }}" required autofocus>
+                            @if ($errors->has('middle_initial'))
+                                <span class="help-block font-italic">
+                                    {{ $errors->first('middle_initial') }}
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }} col-md-5">
+                        <label for="last_name" class="control-label">Last Name</label>
+                            <div class="input-group">
+                                <input id="last_name" type="text" class="form-control" name="last_name" value="{{ old('last_name') }}" required autofocus >
+                                @if ($errors->has('last_name'))
+                                    <span class="help-block font-italic">
+                                        {{ $errors->first('last_name') }}
+                                    </span>
+                                @endif
+                            </div>
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} col-md-7">
+                        <label for="email" class=" control-label">E-Mail Address</label>
+                            <div class="input-group">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block font-italic">
+                                        {{ $errors->first('email') }}
+                                    </span>
+                                @endif
+                            </div>
+                    </div>
+
+                    
+                    <div class="form-group{{ $errors->has('gender') ? ' has-error' : '' }} col-md-5">
+                        <label for="gender" class="control-label">Gender</label>
+                            <div class="row">
+                                <div class="col-md-2"></div>
+                                <div class="col-md-5">
+                                    <input type="radio" name="gender" value="male" checked="checked"><small> Male</small>
+                                </div>
+                                <div class="col-md-5">
+                                    <input type="radio" name="gender" value="female"><small> Female</small>
+                                </div>
+                                @if ($errors->has('gender'))
+                                    <span class="help-block font-italic">
+                                        {{ $errors->first('gender') }}
+                                    </span>
+                                @endif
+                            </div>
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} col-md-6">
+                        <label for="password" class="control-label">Password</label>
+                        <div class="input-group">
+                            <input id="password" type="password" class="form-control" name="password" required>
+                            @if ($errors->has('password'))
+                                <span class="help-block font-italic">
+                                    {{ $errors->first('password') }}
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group col-md-6">
+                        <label for="password-confirm" class="control-label">Confirm Password</label>
+                        <div class="input-group">
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <p class="float-left"><small>Already have an account? <a class="btn-link" href="{{ route('login') }}"><i>Log in</i></a></small></p>
+                <button type="submit" class="btn btn-login float-right">
+                    Register
+                </button>
+                
+               
+            </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!--
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -18,8 +194,8 @@
                                 <input id="first_name" type="text" class="form-control" name="first_name" value="{{ old('first_name') }}" required autofocus>
 
                                 @if ($errors->has('first_name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('first_name') }}</strong>
+                                    <span class="help-block font-italic">
+                                        {{ $errors->first('first_name') }}
                                     </span>
                                 @endif
                             </div>
@@ -32,8 +208,8 @@
                                 <input id="middle_initial" type="text" class="form-control" name="middle_initial" value="{{ old('middle_initial') }}" required autofocus>
 
                                 @if ($errors->has('middle_initial'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('middle_initial') }}</strong>
+                                    <span class="help-block font-italic">
+                                        {{ $errors->first('middle_initial') }}
                                     </span>
                                 @endif
                             </div>
@@ -46,8 +222,8 @@
                                 <input id="last_name" type="text" class="form-control" name="last_name" value="{{ old('last_name') }}" required autofocus>
 
                                 @if ($errors->has('last_name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('last_name') }}</strong>
+                                    <span class="help-block font-italic">
+                                        {{ $errors->first('last_name') }}
                                     </span>
                                 @endif
                             </div>
@@ -61,8 +237,8 @@
                                 <input type="radio" name="gender" value="female"> Female<br>
 
                                 @if ($errors->has('gender'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('gender') }}</strong>
+                                    <span class="help-block font-italic">
+                                        {{ $errors->first('gender') }}
                                     </span>
                                 @endif
                             </div>
@@ -75,8 +251,8 @@
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                    <span class="help-block font-italic">
+                                        {{ $errors->first('email') }}
                                     </span>
                                 @endif
                             </div>
@@ -89,8 +265,8 @@
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
+                                    <span class="help-block font-italic">
+                                        {{ $errors->first('password') }}
                                     </span>
                                 @endif
                             </div>
@@ -116,5 +292,5 @@
             </div>
         </div>
     </div>
-</div>
+</div>-->
 @endsection
