d8fd804e74a2c8c2acf95dc3e8ff1256d56a624e
Amstel <louis_ventanilla@dlsu.edu.ph>
2017-11-29 00:14:09 +0800

    Implement Bootstrap in Account

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,4 +1,4 @@
-@extends('oldlayout.app')
+@extends('layout.app')
 
 @section('title', 'Juang Dela Cruz')
 
@@ -14,6 +14,36 @@
 
   <hr>
   <p>Hi I am Juan and I am a third year college student taking up bachelor of science computer science in De La Salle University</p>
+  <button type="button" class="btn btn-dark">Dark</button>
+
+  <div class="input-group">
+    <span class="input-group-addon" id="basic-addon1">@</span>
+    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+  </div>
+  <br>
+  <div class="input-group">
+    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+    <span class="input-group-addon" id="basic-addon2">@example.com</span>
+  </div>
+  <br>
+  <label for="basic-url">Your vanity URL</label>
+  <div class="input-group">
+    <span class="input-group-addon" id="basic-addon3">https://example.com/users/</span>
+    <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+  </div>
+  <br>
+  <div class="input-group">
+    <span class="input-group-addon">$</span>
+    <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+    <span class="input-group-addon">.00</span>
+  </div>
+  <br>
+  <div class="input-group">
+    <span class="input-group-addon">$</span>
+    <span class="input-group-addon">0.00</span>
+    <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+  </div>
+  
   @endsection
 
 @section('right-sidebar')
