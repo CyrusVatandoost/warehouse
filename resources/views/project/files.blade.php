bda75f7a129c996068275fd62a4d0761126f3506
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-11-29 05:18:40 +0800

    Change Table
    Added verificiation attributes.

--- resources/views/project/files.blade.php
+++ resources/views/project/files.blade.php
@@ -1,30 +1,5 @@
 <div class="tab-pane" id="panel-files">
-
   <p>
-  <nav aria-label="breadcrumb" role="navigation">
-    <ol class="breadcrumb">
-      <li class="breadcrumb-item active" aria-current="page">Home</li>
-    </ol>
-  </nav>
-
-  <div class="card">
-    <div class="card-body">
-      This is some text within a card body.
-    </div>
-  </div>
-  <br>
-  <div class="card">
-    <div class="card-body">
-      This is some text within a card body.
-    </div>
-  </div>
-  <br>
-  <div class="card">
-    <div class="card-body">
-      This is some text within a card body.
-    </div>
-  </div>
-
+  <a href="#" role="button" class="btn btn-primary" data-toggle="modal">Add File</a>
   </p>
-
 </div>
