fd30040886914d617a7ef08c9cb5d53827e582de
Amstel <louis_ventanilla@dlsu.edu.ph>
2017-11-29 01:52:30 +0800

    Change Account To Bootstrap 4.0
    edited the UI for account

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,49 +1,78 @@
 @extends('layout.app')
 
-@section('title', 'Juang Dela Cruz')
 
 @section('left-sidebar')
   <p><a href="#" class="btn btn-primary btn-block">New Announcement</a></p>
   <p><a href="#" class="btn btn-primary btn-block">New Project</a></p>
   @endsection
+<style>
+.banner .container {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+      -ms-flex-align: center;
+          align-items: center;
+  -webkit-box-pack: start;
+      -ms-flex-pack: start;
+          justify-content: flex-start;
+}
+.container {
+  width: 80%;
+  max-width: 900px;
+  margin: 0 auto;
+  height: 100%;
+  -webkit-transition: width .5s ease;
+  transition: width .5s ease;
+}
+.profile-pic {
+  height: 100%;
+  width: 250px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: center;
+      -ms-flex-pack: center;
+          justify-content: center;
+  -webkit-box-align: center;
+      -ms-flex-align: center;
+          align-items: center;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+      -ms-flex-direction: column;
+          flex-direction: column;
+}
+.avatar {
+  background-color: #bbbbbb;
+  height: 190px;
+  min-width: 190px;
+  border-radius: 110px;
+}
+.bio {
+  margin-left: 40px;
+}
 
+
+</style>
 @section('body')
-  <img src="res/account.png" width="150" height="150">
-  <p><a href="/warehouse/account/edit.php">juandelacruz@dlsu.edu.ph</a>
-  <p>T3D Member
 
-  <hr>
-  <p>Hi I am Juan and I am a third year college student taking up bachelor of science computer science in De La Salle University</p>
-  <button type="button" class="btn btn-dark">Dark</button>
-
-  <div class="input-group">
-    <span class="input-group-addon" id="basic-addon1">@</span>
-    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+  <div class="banner">
+  <div class="container">
+    <div class="profile-pic">
+      <div class="avatar"></div>
+      
+    </div>
+    <div class="bio">
+      <h2 class="heading-medium">Juan Dela Cruz</h2>
+      <h5 class="heading-small">TE<sup>3</sup>D Member</h5>
+      <h6 class="heading-small">juan_delacruz@dlsu.edu.ph</h6>
+      <p class="body-small">Hi I'm Juan and I am a third year college student taking up bachelor of science computer science in De La Salle University</p>
+     <button type="button" class="btn btn-primary btn-sm">Edit Profile</button>
   </div>
-  <br>
-  <div class="input-group">
-    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-    <span class="input-group-addon" id="basic-addon2">@example.com</span>
-  </div>
-  <br>
-  <label for="basic-url">Your vanity URL</label>
-  <div class="input-group">
-    <span class="input-group-addon" id="basic-addon3">https://example.com/users/</span>
-    <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
-  </div>
-  <br>
-  <div class="input-group">
-    <span class="input-group-addon">$</span>
-    <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-    <span class="input-group-addon">.00</span>
-  </div>
-  <br>
-  <div class="input-group">
-    <span class="input-group-addon">$</span>
-    <span class="input-group-addon">0.00</span>
-    <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-  </div>
+</div>
   
+
+
   @endsection
 
 @section('right-sidebar')
