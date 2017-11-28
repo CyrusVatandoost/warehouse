1994490d265acbd8ff2bb51b5fa8b544f256f49c
xandraureta <31879731+xandraureta@users.noreply.github.com>
2017-11-29 05:48:54 +0800

    Changed To Bootstrap 4.0

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -17,7 +17,62 @@
 
     <!-- Custom styles for this template -->
     <link href="{{ asset('css/style.css') }}" media="all" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/map.css') }}" media="all" rel="stylesheet" type="text/css" />
     <link href="{{ asset('css/material-icons.css') }}" media="all" rel="stylesheet" type="text/css" />
+
+    <script type="text/javascript">
+          $(document).ready(function() {
+          $(".expandable").on("click", ".add-more", function(e) {
+            e.preventDefault();
+            var rmButton = '<button class="btn btn-danger remove-me" type="button">-</button>';
+            var grandParent = $(this).parent().parent();
+            var countVal = grandParent.data("count");
+            var count = parseInt(countVal);
+            if (count == 1) {
+              $(this).before(rmButton);
+            }
+            var toBeCopied = $(this).parent().clone();
+            if (count == 1) { 
+                var curClass = toBeCopied.find("input").attr('class');
+                toBeCopied.find("input:first").attr('class', curClass + " offset-md-3");
+                toBeCopied.find("label").remove();
+
+            }
+            var add_button = $(this).detach();
+            grandParent.append(toBeCopied);
+            count++;
+            grandParent.data("count", count);
+          });
+          $(".expandable").on("click", ".remove-me", function(e) {
+            e.preventDefault();
+            var grandParent = $(this).parent().parent();
+            var countVal = grandParent.data("count");
+            count = parseInt(countVal);
+            count--;
+            grandParent.data("count", count);
+
+            var nextButton = $(this).next("button");
+            var prevButton = $(this).parent().prev().find("button");
+
+            //When we click remove on the last entry:
+            if (/add-more/.test(nextButton.attr("class")) && /remove-me/.test(prevButton.attr("class"))) {
+              var add_button = nextButton.detach();
+              $(this).parent().prev().find(".remove-me").after(add_button);
+            }
+            //When we click on the first entry:
+            if ($(this).parent().children().is("label")) {
+                secondEntry=$(this).parent().next().find("input");
+                secondEntry.removeClass("offset-md-3");
+                secondEntry.before($(this).parent().find("label"));
+            }
+            if (count == 1) {
+              $(this).parent().prev().find(".remove-me").remove();
+              $(this).parent().next().find(".remove-me").remove();
+            }
+            $(this).parent().remove();
+          });
+        });
+</script>
 
   </head>
 
@@ -32,7 +87,7 @@
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a class="nav-link" href="/home">Home</a>
+            <a class="nav-link" href="/sample">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/projects">Projects</a>
@@ -97,7 +152,7 @@
 
         <!-- body -->
         <div class="col-sm-8 text-left"> 
-          <h1>@yield('title')</h1>
+          <h1>@yield('page-title')</h1>
           @yield('body')
         </div>
 
