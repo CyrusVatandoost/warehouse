5a967fef97a75b681a70c98e36d0bd664cb7dcad
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-12-02 18:31:16 +0800

    Fix Adding Collaborator Through ID

--- app/Http/Controllers/CollaboratorController.php
+++ app/Http/Controllers/CollaboratorController.php
@@ -8,21 +8,15 @@ use App\Collaborator;
 
 class CollaboratorController extends Controller {
  
-	public function store(Project $project) {
+	public function store($project_id) {
 
-		/*
-		$collaborator = new Collaboration;
-		$collaborator->project_id = $project->project_id;
+		$collaborator = new Collaborator;
+		$collaborator->project_id = $project_id;
 		$collaborator->user_id = request('user_id');
 		$collaborator->save();
-		*/
-
-		Collaborator::create([
-				'project_id' => $project->project_id,
-				'user_id' => request('user_id')
-		]);
 
 		return back();
+		
 	}
 
 }
