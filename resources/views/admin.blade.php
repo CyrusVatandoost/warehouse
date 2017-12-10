17937638114f83724f890f28ffed45154543376d
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-12-10 16:10:04 +0800

    Fix Add Collaborator Bug

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -27,7 +27,6 @@
   <p><a href="#modal-container-new-announcement" role="button" class="btn btn-primary btn-block" data-toggle="modal">New Announcement</a></p>
   <p><a href="#modal-container-new-project" role="button" class="btn btn-primary btn-block" data-toggle="modal">New Project</a></p>
   <p><a href="#modal-container-select-featured-project/s" role="button" class="btn btn-primary btn-block" data-toggle="modal">Select Featured Project/s</a></p>
-  <p><a href="#modal-container-delete-account" role="button" class="btn btn-primary btn-block" data-toggle="modal">Delete an Account</a></p>
   <p><a href="/admin/archive" role="button" class="btn btn-primary btn-block">Archive</a></p>
 @endsection
 
