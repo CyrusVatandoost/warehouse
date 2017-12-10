0b87e48968bccb0ea22a9530280c7071c4603bc9
dflachica <daniel.lachica82@gmail.com>
2017-12-10 19:17:16 +0800

    added account actions to logs
    update profile pic, update bio, and update personal info (fname, lname,MI, and gender) added to warehousedb.logs

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,17 +5,31 @@ namespace App\Http\Controllers;
 use Storage;
 use Input;
 use App\User;
+use App\Log;
 use Illuminate\Http\Request;
 
 class UserController extends Controller {
 
  	public function updateAvatar(Request $request, $id) {
-    $user = User::find($id)->first();
+    $user = User::find($id);
 
     $image = $request->file('profile_pic');
     $input['name'] = $id.'.'.$image->getClientOriginalExtension();
     $destinationPath = public_path('/storage/avatars');
     $image->move($destinationPath, $input['name']);
+
+    //add update action to logs table
+    $log = new Log;
+
+    $log->user_id = $id;
+    if($user->gender == 'Male')
+      $log->user_action = "updated his profile picture to";
+    else
+      $log->user_action = "updated her profile picture to";
+
+    $log->action_details = $image;
+    $log->save();
+    //end log
 
     return back();
   	}
@@ -24,6 +38,18 @@ class UserController extends Controller {
   		$user = User::find($id);
 
   		$user->bio = request('profile_bio');
+
+      //add update action to logs table
+      $log = new Log;
+
+      $log->user_id = $id;
+      if($user->gender == 'Male')
+        $log->user_action = "updated his profile";
+      else if($user->gender == 'Female')
+        $log->user_action = "updated her profile";
+      $log->action_details = "Biography";
+      $log->save();
+      //end log
 
   		$user->save();
   		return redirect ('/account');
@@ -40,6 +66,24 @@ class UserController extends Controller {
       $user->gender = request('gender');
 
       $user->save();
+
+      //add update action to logs table
+      $log = new Log;
+
+      $log->user_id = $id;
+
+      // NOTE: For some reason, gender isn't working here, but it works with updateBio(). Instead of going with "him" or "her", i went with "their" at the moment.
+
+      // if($user->gender == 'Male')
+      //   $log->user_action = "updated his profile";
+      // else if($user->gender == 'Female')
+      //   $log->user_action = "updated her profile";
+
+      $log->user_action = "updated their profile";
+      $log->action_details = "Personal Information";
+      $log->save();
+      //end log
+
       return redirect('/account');
     }
 }
