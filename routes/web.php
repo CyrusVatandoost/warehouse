b6464cd51375ebab3bdc7858242e61ff6479f81a
Marl Christian Ricanor <32050672+Marlcelo@users.noreply.github.com>
2017-12-10 22:45:20 +0800

    Add Admins through user ID
    This update lets admins promote users to admins by simply typing their user id and click the button. It is automatically saved in the database as well

--- routes/web.php
+++ routes/web.php
@@ -79,6 +79,7 @@
 	Route::get('/archive/delete/{file}', 'FileController@deleteArchive');
 	Route::get('/archive/restore/{file}', 'FileController@restoreArchive');
 	Route::get('/admin/approve/{id}/mail/{email}', 'AdminController@approveUser');
+	Route::post('/admin/store','AdminController@store');
 
 // login and register
 	Auth::routes();
