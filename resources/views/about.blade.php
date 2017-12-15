b0320f6906d6f555341dba09c434e316d1d988ee
xandraureta <31879731+xandraureta@users.noreply.github.com>
2017-12-15 15:00:04 +0800

    Add About Us Page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,13 +6,15 @@
 
 <!-- title at tab -->
 @section('title', 'About Us')
-<!-- title at body -->
-@section('page-title', 'About Us')
 
 <!-- css styles -->
 @section('style')
 	<!-- insert custom css styles here -->
 	<!-- i suggest to avoid custom css styles and have it in the .css file in `public/css` -->
+	<style>
+	
+
+	</style>
 @endsection
 
 <!-- modals -->
@@ -28,20 +30,43 @@
 <!-- body -->
 @section('body')
   <!-- insert body here -->
-  <p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in velit facilisis, sollicitudin sapien at, faucibus ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam tristique fermentum est et gravida. Fusce semper nisi sed eros condimentum, et rhoncus tellus viverra. Praesent consequat pellentesque lorem non suscipit. Ut accumsan, lacus et pharetra malesuada, mi metus hendrerit massa, in commodo dolor justo sit amet lacus. Vestibulum finibus mattis nisl non sagittis. Mauris nisi tellus, pretium vitae fringilla eget, finibus sit amet lacus.
+  
+ 
 
-	<p>
-	Vestibulum consectetur rutrum pharetra. Sed a erat pellentesque, molestie augue sed, imperdiet dui. Duis cursus placerat nibh, tempor auctor nulla tempor sit amet. Suspendisse ipsum ex, blandit at accumsan vehicula, lobortis nec enim. Cras commodo tellus nibh, non lacinia mi sollicitudin eu. Cras accumsan tellus lorem, at fermentum arcu venenatis ac. Nunc malesuada sagittis justo id condimentum. Nullam imperdiet mollis lectus nec porttitor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed bibendum lacinia commodo.
-
-	<p>
-	Sed ultricies pulvinar metus in sodales. Duis mattis varius quam. In eget facilisis sapien, at pellentesque magna. Quisque non justo nunc. Fusce id nulla in neque pulvinar semper non vitae orci. Praesent varius neque eget tellus pellentesque, sit amet bibendum erat tincidunt. Cras rutrum, orci id lacinia sagittis, lectus nulla tempus nulla, et fringilla ante turpis sit amet leo. Sed pretium neque justo, eu maximus nunc vehicula ac.
-
-	<p>
-	Nunc lacus enim, dictum sit amet augue at, sagittis eleifend sem. Fusce eget porta odio. Donec nec dictum massa. Vestibulum consectetur nisl non leo accumsan, nec sagittis ipsum cursus. Suspendisse enim odio, dapibus eu felis in, maximus gravida erat. Maecenas eu luctus nisl. Proin bibendum lectus eu rhoncus vulputate. Sed laoreet elit a enim feugiat, eget aliquam ligula sodales. Etiam non lectus risus. Vestibulum convallis in sapien eu malesuada. Nulla a justo vitae erat ultricies vehicula. Vestibulum id ipsum dapibus libero ullamcorper ultricies sed nec urna. Ut risus quam, euismod efficitur eros nec, dignissim tincidunt neque. Donec sit amet lacus nec lectus imperdiet cursus et nec nunc. Fusce finibus purus in sodales molestie.
-
-	<p>
-	Aliquam finibus sapien luctus leo hendrerit lobortis. Etiam at pellentesque sem. Integer commodo mi ut lacus sollicitudin, nec tempor mauris hendrerit. Duis metus sapien, auctor vel posuere ut, mollis quis tellus. Duis non justo in ex lobortis ultrices ac sed sem. Vestibulum dapibus ornare nulla sit amet accumsan. Vivamus convallis lacus sed lectus interdum feugiat. Etiam ipsum neque, viverra sed leo id, pulvinar ultricies odio. Vestibulum vestibulum ipsum a eros elementum, eget sodales ante fringilla. Aliquam vitae iaculis orci. Praesent est lacus, elementum a dui sit amet, varius accumsan orci. Suspendisse potenti. Praesent rhoncus urna vitae mauris convallis vestibulum. Integer at neque id quam aliquam pretium. Pellentesque eu arcu tempus, euismod odio eget, consectetur dolor.
+ <div class="container">
+      <div class="row text-center">
+        <div class="col-md-12">
+          <h1 class="mb-4 text-black">Benefits and features</h1>
+          <p class="lead text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor iaculis purus. In egestas nisi mauris, convallis accumsan ipsum elementum a. Curabitur aliquam viverra egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam viverra vel felis ac iaculis. Fusce malesuada luctus odio vitae semper. Nunc imperdiet tempus ullamcorper. Pellentesque vehicula, turpis id ullamcorper ornare, urna velit luctus lacus, et hendrerit erat ex ac ex. Donec ac dui bibendum, auctor nulla a, finibus sem. Pellentesque at cursus urna. Suspendisse congue, dolor vel sagittis consectetur, mauris nisl ullamcorper odio, eget ultrices diam lectus in sapien.</p>
+          <div class="row text-left mt-5">
+            <div class="col-md-4 my-3">
+              <div class="row mb-3">
+                <div class="align-self-center col-12">
+                  <h5 class="text-secondary"><b>For the community</b></h5>
+                </div>
+              </div>
+              <p>Nullam blandit bibendum ornare. Etiam imperdiet leo nisl. Vivamus volutpat ante quam, sit amet luctus nisl venenatis vel. Vivamus eget leo eu erat accumsan posuere quis nec turpis. Phasellus luctus convallis imperdiet. Nunc eget pharetra diam. Curabitur eleifend mi id rhoncus egestas. Curabitur in felis quis metus accumsan tristique. Donec consectetur urna nunc, at dignissim ex vulputate vitae. Praesent et nunc ipsum. Pellentesque sed dolor eget magna rutrum hendrerit.</p>
+            </div>
+            <div class="col-md-4 my-3">
+              <div class="row mb-3">
+                <div class="align-self-center col-12">
+                  <h5 class="text-secondary"><b>For the school</b></h5>
+                </div>
+              </div>
+              <p>Nullam blandit bibendum ornare. Etiam imperdiet leo nisl. Vivamus volutpat ante quam, sit amet luctus nisl venenatis vel. Vivamus eget leo eu erat accumsan posuere quis nec turpis. Phasellus luctus convallis imperdiet. Nunc eget pharetra diam. Curabitur eleifend mi id rhoncus egestas. Curabitur in felis quis metus accumsan tristique. Donec consectetur urna nunc, at dignissim ex vulputate vitae. Praesent et nunc ipsum. Pellentesque sed dolor eget magna rutrum hendrerit.</p>
+            </div>
+            <div class="col-md-4 my-3">
+              <div class="row mb-3">
+                <div class="align-self-center col-12">
+                  <h5 class="text-secondary"><b>For the greater good</b></h5>
+                </div>
+              </div>
+              <p>Nullam blandit bibendum ornare. Etiam imperdiet leo nisl. Vivamus volutpat ante quam, sit amet luctus nisl venenatis vel. Vivamus eget leo eu erat accumsan posuere quis nec turpis. Phasellus luctus convallis imperdiet. Nunc eget pharetra diam. Curabitur eleifend mi id rhoncus egestas. Curabitur in felis quis metus accumsan tristique. Donec consectetur urna nunc, at dignissim ex vulputate vitae. Praesent et nunc ipsum. Pellentesque sed dolor eget magna rutrum hendrerit.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
 @endsection
 
