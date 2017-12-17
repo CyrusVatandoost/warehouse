3b43141b64b2fad394e13f994d9eac6301f0bd1f
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-12-17 21:31:22 +0800

    Added Seeder
    Added:
    - user faker
    - project faker
    - announcement faker
    
    Fix:
    - guest projects page

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use Faker\Factory as Faker;
+use App\Announcement;
+use App\User;
+use App\Project;
 
 class DatabaseSeeder extends Seeder {
 
@@ -14,14 +17,34 @@ class DatabaseSeeder extends Seeder {
   public function run() {
 
     $faker = Faker::create();
-    foreach (range(1,10) as $index) {
-        DB::table('users')->insert([
-            'first_name' => $faker->name,
-            'middle_initial' => $faker->letter,
-            'last_name' => $faker->name,
-            'email' => $faker->email,
-            'password' => bcrypt('secret'),
-        ]);
+    foreach (range(1,50) as $index) {
+      DB::table('users')->insert([
+        'first_name' => $faker->firstname,
+        'middle_initial' => 'A.', 
+        'last_name' => $faker->lastname,
+        'email' => $faker->email,
+        'password' => bcrypt('secret'),
+        'bio' => $faker->paragraph,
+      ]);
+    }
+
+    $users = User::all()->pluck('user_id')->toArray();
+    foreach(range(1, 300) as $index) {
+      $project = Project::create([
+        'user_id' => $faker->randomElement($users),
+        'name' => $faker->catchPhrase,
+        'description' => $faker->paragraph,
+        'public' => $faker->randomElement([ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ]),
+      ]);
+    }
+
+    foreach(range(1, 150) as $index) {
+      $announcement = Announcement::create([
+        'user_id' => $faker->randomElement($users),
+        'name' => $faker->bs,
+        'description' => $faker->paragraph,
+        'expires_on' => $faker->date,
+      ]);
     }
 
   }
